bbec2e759c5e300c01132dcad5aa7bceedbbdb69
Felipe <felipeandres_uc@outlook.com>
2019-06-10 14:54:23 -0400

    add form and validate

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,19 @@
 class SalesController < ApplicationController
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(post_params)
+    @sale.save
+    redirect_to posts_path
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:cod, :detail, :category, :value, :discount, :tax, :total)
+  end
+
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,8 @@
 class Sale < ApplicationRecord
+  validates :cod, presence: true, uniqueness: true
+  validates :detail, presence: true
+  validates :category, inclusion: { in: %w(uno dos tres cuatro cinco),
+  message: "%{value} is not a valid size" }
+  validates :value, numericality: { only_integer: true }
+  validates :discount, numericality: { only_integer: true, less_than_or_equal_to: 40}
 end
