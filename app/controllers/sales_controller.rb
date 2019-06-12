5a4d0829a13a97832a4b9fc46f28fece0a836113
Felipe <felipeandres_uc@outlook.com>
2019-06-11 23:26:37 -0400

    fix def create

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,18 +2,41 @@ class SalesController < ApplicationController
 
   def new
     @sale = Sale.new
+    @categories = ["uno","dos","tres","cuatro"]
   end
 
   def create
+
     @sale = Sale.new(post_params)
+
+    if @sale.discount > 0
+       @discount = (@sale.value) - (@sale.value * @sale.discount/100)
+
+
+    end
+
+    if @sale.tax == 0
+
+        @sale.tax = 19
+
+    else
+        @sale.tax = 0
+
+    end
+
+    @sale.total = @sale.value - (@sale.tax*@discount/100)
+
     @sale.save
-    redirect_to posts_path
+    redirect_to sales_done_path
   end
 
+  def done
+    @sales = Sale.all
+  end
 
   private
   def post_params
-    params.require(:post).permit(:cod, :detail, :category, :value, :discount, :tax, :total)
+    params.require(:sale).permit(:cod, :detail, :category, :value, :discount, :tax)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'sales/new'
-  post 'sales', to: 'sales#create'
+  #get 'sales/new'
+  #post 'sales', to: 'sales#create'
+  get 'sales/done'
+  resources :sales
 end
